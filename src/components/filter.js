a993826d432e4ce6e548e90f09a0f59cf764e4c2
TatarPrincess <ksusha.bukovskaya@gmail.com>
2019-08-28 17:13:04 +0300

    8. Личный проект: и ехать и шашечки

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,7 +1,7 @@
-export const getFilterMarkup = () => `<nav class="main-navigation">
-                                      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-                                      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-                                      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-                                      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+export const getFilterMarkup = ([all, watchList, history, favorities]) => `<nav class="main-navigation">
+                                      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies <span class="main-navigation__item-count">${all.count}</span></a>
+                                      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchList.count}</span></a>
+                                      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${history.count}</span></a>
+                                      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorities.count}</span></a>
                                       <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
                                     </nav>`;
--- src/main.js
+++ src/main.js
@@ -3,25 +3,70 @@ import {getFilterMarkup} from './components/filter.js';
 import {getSearchMarkup} from './components/search.js';
 import {getUserRankMarkup} from './components/userRank.js';
 import {getSortMarkup} from './components/sort.js';
+import {getCardContainerMarkup} from './components/cardContainer.js';
+import {getFilmData} from './components/data.js';
+import {getFilterData} from './components/data.js';
+import {getFooterStatMarkup} from './components/footerStat.js';
 
-function Component(creator, adjEl, order) {
+const CARDS_TOTAL_QUANTITY = 15;
+const CARDS_LOAD_QUANTITY_PORTION = 5;
+const USER_RANK = `Movie Buff`;
+let cardDataArr = [];
+let loadedCardsQuantity = 0;
+let cardDataArrPortion = [];
+let lastPortion = 0;
+
+function Component(creator, adjEl, order, elOrder, params = ``) {
   this.adjEl = adjEl;
   this.creator = creator;
-  this.markup = creator();
+  this.markup = creator(params);
   this.drawOrder = order;
+  this.drawElOrder = elOrder;
 }
 
+const getFilmCardsList = (isLoadMore = false) => {
+
+  if (!isLoadMore) {
+    for (let i = 1; i <= CARDS_TOTAL_QUANTITY; i++) {
+      cardDataArr.push(getFilmData());
+    }
+  }
+
+  cardDataArrPortion = cardDataArr.slice(loadedCardsQuantity, CARDS_LOAD_QUANTITY_PORTION + loadedCardsQuantity);
+
+  if (!cardDataArrPortion.length) {
+    return ``;
+  }
+
+  let cardMarkupTotal = ``;
+  cardDataArrPortion.forEach((item) => {
+    cardMarkupTotal = cardMarkupTotal + getFilmCardMarkup(item);
+    loadedCardsQuantity++;
+  });
+  if ((CARDS_TOTAL_QUANTITY - loadedCardsQuantity) < CARDS_LOAD_QUANTITY_PORTION) {
+    lastPortion = 1;
+  }
+  cardDataArrPortion.splice(0, cardDataArrPortion.length);
+
+  return isLoadMore ? cardMarkupTotal : getCardContainerMarkup(cardMarkupTotal);
+};
+
 const markupObjArray = [];
+const getFiltersMarkup = () => getFilterMarkup(getFilterData(cardDataArr));
 
 function fillMarkupObjArray() {
   const adjElForSearch = document.querySelector(`.header`);
   const adjMainEl = document.querySelector(`.main`);
+  const adjFooterEl = document.querySelector(`.footer`);
 
-  markupObjArray.push(new Component(getSearchMarkup, adjElForSearch, `beforeend`));
-  markupObjArray.push(new Component(getUserRankMarkup, adjElForSearch, `beforeend`));
-  markupObjArray.push(new Component(getFilterMarkup, adjMainEl, `beforeend`));
-  markupObjArray.push(new Component(getSortMarkup, adjMainEl, `beforeend`));
-  markupObjArray.push(new Component(getFilmCardMarkup, adjMainEl, `beforeend`));
+  markupObjArray.push(new Component(getSearchMarkup, adjElForSearch, `beforeend`, 0));
+  markupObjArray.push(new Component(getUserRankMarkup, adjElForSearch, `beforeend`, 1, USER_RANK));
+  markupObjArray.push(new Component(getFilmCardsList, adjMainEl, `beforeend`, 4));
+  markupObjArray.push(new Component(getFiltersMarkup, adjMainEl, `beforeend`, 2));
+  markupObjArray.push(new Component(getSortMarkup, adjMainEl, `beforeend`, 3));
+  markupObjArray.push(new Component(getFooterStatMarkup, adjFooterEl, `beforeend`, 5, CARDS_TOTAL_QUANTITY));
+
+  markupObjArray.sort((el1, el2) => el1.drawElOrder - el2.drawElOrder);
 }
 
 // рендер компонент
@@ -33,4 +78,14 @@ fillMarkupObjArray();
 
 markupObjArray.forEach(function (item) {
   render(item.adjEl, item.markup, item.drawOrder);
+});
+
+const loadMoreEl = document.querySelector(`.films-list__show-more`);
+const filmContainerEl = document.querySelector(`.films-list__container`);
+loadMoreEl.addEventListener(`click`, () => {
+  const markupLoadMoreTasks = getFilmCardsList(true);
+  render(filmContainerEl, markupLoadMoreTasks, `beforeend`);
+  if (lastPortion) {
+    loadMoreEl.className = `films-list__show-more visually-hidden`;
+  }
 });
